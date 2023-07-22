5de67ec6afeaba8c6f683325bb8434e8b7e60215
Iqra Imtiaz <iqra_imtiaz09@hotmail.com>
2023-07-22 15:52:49 -0700

    adding deepface emotion detection with dash

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # Mlops-Final-Project
+# Emotion Detection App
+
+This application detects human emotions from uploaded images using the DeepFace model for facial emotion recognition. The app is developed using the Dash framework by Plotly.
+
+## Features
+
+- Upload an image: Users can upload an image using the upload button. The app currently supports one image at a time.
+- View uploaded image: Once the image is uploaded, it is displayed on the screen.
+- Emotion detection: After uploading, the image is analyzed immediately, and the detected emotions are displayed in a table below the image.
+
+![Alt Text](demo.png)
+
+## Installation
+
+Make sure you have the following installed:
+
+- Python 3.7+
+- pip (Python package manager)
+
+### Setup
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/yourgithubusername/emotion-detection-app.git
+cd emotion-detection-app
+```
+
+### Install the dependencies:
+
+`pip install -r requirements.txt
+`
+
+### Running the App
+1. Navigate to the project's root directory.
+2. Run the app:
+`python app.py
+`
+1. Visit http://127.0.0.1:8050 in your web browser.
--- app.py
+++ app.py
@@ -1,23 +1,82 @@
-from flask import Flask, request, jsonify
-import os
+import dash
+from dash import html, dcc
+from dash.dependencies import Input, Output, State
+import dash_bootstrap_components as dbc
+import dash_table
+import base64
+import io
+from PIL import Image
+from deepface import DeepFace
+import pandas as pd
 
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'static/uploads'  # Specify the folder to store uploaded images
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-@app.route('/upload', methods=['POST'])
-def upload():
-    if 'image' not in request.files:
-        return jsonify({'error': 'No image found'}), 400
-    
-    file = request.files['image']
-    if file.filename == '':
-        return jsonify({'error': 'No image selected'}), 400
+app.layout = html.Div([
+    html.H1('Emotion Detector'),
+    html.P("This application detects human emotions from uploaded images. Please upload an image and see the result."),
+    dcc.Upload(
+        id='upload-image',
+        children=html.Div([
+            'Drag and Drop or ',
+            html.A('Select an Image')
+        ]),
+        style={
+            'width': '100%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px'
+        },
+        # Allow multiple files to be uploaded
+        multiple=False
+    ),
+    html.Div(id='output-image-upload'),
+])
 
-    # Save the uploaded image to the designated folder
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-    
-    # Return the URL of the uploaded image
-    return jsonify({'url': f'/uploads/{file.filename}'}), 200
+
+def parse_contents(contents, filename):
+    content_type, content_string = contents.split(',')
+    decoded = base64.b64decode(content_string)
+    try:
+        im = Image.open(io.BytesIO(decoded))
+        im.save('tmp/' + filename)
+        result = DeepFace.analyze('tmp/' + filename, actions=['emotion'])
+        emotion = result[0]['emotion']
+        df = pd.DataFrame([emotion.values()], columns=emotion.keys())
+
+    except Exception as e:
+        print(e)
+        return html.Div([
+            'There was an error processing this file.'
+        ])
+    return html.Div([
+        html.H5(filename),
+        # HTML images accept base64 encoded strings in the same format
+        html.Img(src=contents, style={'width': '300px'}),
+        dash_table.DataTable(
+            data=df.to_dict('records'),
+            columns=[{'name': i, 'id': i} for i in df.columns],
+            style_cell={'textAlign': 'left'},
+            style_header={
+                'backgroundColor': 'rgb(230, 230, 230)',
+                'fontWeight': 'bold'
+            }
+        ),
+        html.Hr(),
+    ])
+
+
+@app.callback(Output('output-image-upload', 'children'),
+              Input('upload-image', 'contents'),
+              State('upload-image', 'filename'))
+def update_output(contents, name):
+    if contents is not None:
+        children = parse_contents(contents, name)
+        return children
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run_server(debug=True)
